dc30f3a6c0c04367e0f78928971ec23575f93f12
Doemuu <doemu@outlook.com>
2022-12-19 01:59:44 +0100

    ✨ Create Navigationtoggle and burgermenu

--- components/layout.vue
+++ components/layout.vue
@@ -1,17 +1,35 @@
 <template>
   <div>
-    <slot />
-    <Footer/>
+    <Burger :isActive="navigationTriggered" @navToggle="handleNavToggle" />
+    <div v-if="navigationTriggered">
+      <p>Hello Nav</p>
+    </div>
+    <div v-else>
+      <slot />
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
 import Footer from './footer.vue'
+import Burger from './navigation/burger.vue'
 
 export default {
   name: 'Layout',
   components: {
     Footer,
+    Burger,
+  },
+  data() {
+    return {
+      navigationTriggered: false,
+    }
+  },
+  methods: {
+    handleNavToggle() {
+      this.navigationTriggered = !this.navigationTriggered
+    },
   },
 }
 </script>
